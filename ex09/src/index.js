13ae14f1b36148491f8519a5dbae609d93e2eb8f
demon034 <sspahic2@etf.unsa.ba>
2021-03-10 18:11:08 +0100

    Difference

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var difference; 
+    difference = numOne - numTwo;
+    
+    // Only change code above this line
+    return difference;
+}
+
+console.log(main(425, 120));  // change this line
+
+module.exports = main;
