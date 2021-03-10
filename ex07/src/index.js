21302d35e4b635650a541e0da9e7d5be2812d889
demon034 <sspahic2@etf.unsa.ba>
2021-03-10 18:04:19 +0100

    Proper camelCase

---
+++ ex07/src/index.js
@@ -0,0 +1,24 @@
+function main() {
+    // Only change code below this line
+    
+    // Variable declarations
+    var stupidlyCapVar;
+    var properCamelCase;
+    var titleCaseCover;
+    
+    // Variable assignments
+    stupidlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseCover = 9000;
+
+    // Only change code above this line
+    return {
+        stupidlyCapVar,
+        properCamelCase,
+        titleCaseCover
+    }
+}
+
+console.log(main());
+
+module.exports = main;
