69dfa8efca9610e19f1b6d381e174c2d84174ec6
demon034 <sspahic2@etf.unsa.ba>
2021-03-10 17:39:55 +0100

    Comment types

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    // Single-line comment
+    /*
+        Multi-line comment
+    */
+    // Only change code above this line
+
+}
+
+main();
+
+module.exports = main;
